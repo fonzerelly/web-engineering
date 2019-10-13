f17e76e98518c7fb4674332cff19d8dbb7804f4f
cs8433 <christian.hoerauf@consorsbank.de>
2019-10-14 00:46:17 +0200

    solidify release

--- release.sh
+++ release.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
+set -e
 echo ------------ start release --------------
 npm version minor
+echo ***** versioning done
 git checkout gh-pages
-git merge master
+echo ***** in gh-pages
+git merge master -X theirs
+echo ***** master merged
 sed -i -- 's/node_modules\/reveal\.js/https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/reveal.js\/3.6.0/g' index.html
+echo ***** replaced node_modules by cdn
 git add index.html
+echo adding index.html
 git commit -m "Auto: replace node_modules with CDN"
+echo commiting index.html
 git push origin gh-pages
+echo releasing...
 git checkout master
 echo ------------ release finished ------------
  
