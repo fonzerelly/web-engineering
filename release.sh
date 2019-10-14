7b4288bc0cd9384653a400ce6384884ecc4be127
cs8433 <christian.hoerauf@consorsbank.de>
2019-10-14 19:20:14 +0200

    activate controls for released version

--- release.sh
+++ release.sh
@@ -8,6 +8,8 @@ echo ***** in gh-pages
 git merge master -X theirs
 echo ***** master merged
 sed -i -- 's/node_modules\/reveal\.js/https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/reveal.js\/3.6.0/g' index.html
+echo ***** activate controls
+sed -i -- 's/controls: false/controls: true/g' index.html
 echo ***** replaced node_modules by cdn
 git add index.html
 echo adding index.html
